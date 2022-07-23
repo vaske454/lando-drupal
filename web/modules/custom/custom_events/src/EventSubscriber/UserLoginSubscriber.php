7c323daeee3e5ccb193f45b182715367433317aa
vaske454 <vasilijetomovic98@gmail.com>
2022-07-23 13:22:21 +0200

    Created project and event subscriber module.

---
+++ web/modules/custom/custom_events/src/EventSubscriber/UserLoginSubscriber.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Drupal\custom_events\EventSubscriber;
+
+use Drupal\custom_events\Event\UserLoginEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Class UserLoginSubscriber.
+ *
+ * @package Drupal\custom_events\EventSubscriber
+ */
+class UserLoginSubscriber implements EventSubscriberInterface {
+
+  /**
+   * Database connection.
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
+  protected $database;
+
+  /**
+   * Date formatter.
+   *
+   * @var \Drupal\Core\Datetime\DateFormatterInterface
+   */
+  protected $dateFormatter;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    return [
+      // Static class constant => method on this class.
+      UserLoginEvent::EVENT_NAME => 'onUserLogin',
+    ];
+  }
+
+  /**
+   * Subscribe to the user login event dispatched.
+   *
+   * @param \Drupal\custom_events\Event\UserLoginEvent $event
+   *   Dat event object yo.
+   */
+  public function onUserLogin(UserLoginEvent $event) {
+    $database = \Drupal::database();
+    $dateFormatter = \Drupal::service('date.formatter');
+
+    $account_created = $database->select('users_field_data', 'ud')
+      ->fields('ud', ['created'])
+      ->condition('ud.uid', $event->account->id())
+      ->execute()
+      ->fetchField();
+
+    \Drupal::messenger()->addStatus(t('Welcome, your account was created on %created_date.', [
+      '%created_date' => $dateFormatter->format($account_created, 'short'),
+    ]));
+  }
+
+}
